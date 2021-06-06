1722d12d5772722c1bd890f99b4f2ed22bd0167b
acup1 <68469692+acup1@users.noreply.github.com>
2021-06-06 19:57:34 +0300

    Update disframes.js

--- disframes.js
+++ disframes.js
@@ -6,7 +6,7 @@ e.innerHTML="<a style='position:absolute;z-index:100;top:100px;' onclick='create
 
 function createFrame(link){
   e=getElementByXpath("/html/body/div/div[2]")
-  css="<style>#qwe {position:absolute;z-index:100 ;background-color:#f1f1f1;text-align:center;top:200px;right:200px;width:720px;height:480px;} .fb {color:black;vertical-align: middle;}</style>"
+  css="<style>#qwe {position:absolute;z-index:100 ;background-color:#f1f1f1;text-align:center;top:200px;right:200px;width:720px;height:480px;} .fb {font-size: 14px;color:black;vertical-align: middle;}</style>"
   html="<div id='qwe'><div style='background-color:red;height:20px;text-align: right;'><a class='fb'>o</a><a class='fb' style='margin-right:5px;' onclick='"+'$("#qwe").remove()'+"'>x</a></div>"+"<iframe is='x-frame-bypass' width=100% height=100% src='"+link+"'></iframe>"+"</div>"
   e.innerHTML=css+html+e.innerHTML;
   setTimeout('$("#qwe").draggable();$("#qwe").resizable()',500);
